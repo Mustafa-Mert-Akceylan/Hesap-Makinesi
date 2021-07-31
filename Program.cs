d0d72fc2852e5e14345997f1e67207c09821ebd8
M² <80332192+Mustafa-Mert-Akceylan@users.noreply.github.com>
2021-07-31 18:19:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Konsol_Dersleri_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int sayi1, sayi2, sonuc;
+            string islem;
+            Console.WriteLine("==========Hesap Makinesi==========");
+            Console.WriteLine("1. Sayıyı Giriniz");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2. Sayıyı Giriniz");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("İşlemi Giriniz");
+            islem = Console.ReadLine();
+            if (islem == "+")
+            {
+                sonuc = sayi1 + sayi2;
+                Console.Clear();
+                Console.WriteLine(sonuc);
+            }
+            else if (islem == "-")
+            {
+                sonuc = sayi1 - sayi2;
+                Console.Clear();
+                Console.WriteLine(sonuc);
+            }
+            else if (islem == "*")
+            {
+                sonuc = sayi1 * sayi2;
+                Console.Clear();
+                Console.WriteLine(sonuc);
+            }
+            else if (islem == "/")
+            {
+                sonuc = sayi1 / sayi2;
+                Console.Clear();
+                Console.WriteLine(sonuc);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("HATA");
+            }
+            
+        }
+    }
+}
